b1adc17a377349fe4b36cb8dba981da6cccd0b73
possiblydhila <fadhilapurboningrum@gmail.com>
2024-06-05 20:28:58 +0700

    Develop toggle todo feature

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+// Definisikan function toggleCompleted di sini
+const toggleCompleted = () => {
+    console.log('toggleCompleted function is called')
+  }
+// Menerima function toggleCompleted sebagai sebuah prop
+const TodoItem = ({ todo, toggleCompleted }) => {
+    const getTodoTitleStyle = () => {
+      if (todo.completed === true) {
+        return { textDecoration: 'line-through' }
+      } else {
+        return { textDecoration: 'none' }
+      }
+    }
+
+    
+
+  
+    return (
+        <div style={styles.todoItem}>
+          <input
+            type="checkbox"
+            style={styles.checkbox}
+            //  Memberikan id dari todo sebagai argument
+            onChange={() => toggleCompleted(todo.id)}
+          />
+          <p style={getTodoTitleStyle()}>{todo.title}</p>
+        </div>
+      )
+  }
+  
+  const styles = {
+    todoItem: {
+      border: '2px solid #f4f4f4',
+      fontSize: '24px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    checkbox: {
+        marginRight: '10px',
+        height: '18px',
+        width: '18px',
+      },
+  }
+
+export default TodoItem
---
+++ src/components/Todos.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+// Menerima function toggleCompleted sebagai sebuah prop
+const Todos = ({ todos, toggleCompleted }) => {
+    return (
+      <div style={styles.container}>
+        {todos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              // Teruskan function toggleCompleted ke component TodoItem
+              toggleCompleted={toggleCompleted}
+            />
+          )
+        })}
+      </div>
+    )
+  }
+  
+  const styles = {
+    container: {
+      width: '40%',
+      margin: '0 auto',
+    //   padding: '0px 100px 0px 100px'
+    },
+  }
+// const Todos = (props) => {
+//     return (
+//       <div>
+//         {props.todos.map((todo) => {
+//           return <p key={todo.id}>{todo.title}</p>
+//         })}
+//       </div>
+//     )
+//   }
+
+export default Todos
